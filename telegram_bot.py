3badb738c83b1d18cf1a293fc59fee23b08f016b
Glaucio Campos <69731354+glauciocampos@users.noreply.github.com>
2022-12-13 12:50:42 -0300

    Fix telegram_bot.py e .env.example

--- telegram_bot.py
+++ telegram_bot.py
@@ -29,6 +29,14 @@ S_MSG = os.environ.get("S_MSG")
 S_ERR_MSG = os.environ.get("S_ERR_MSG")
 KB_S_MSG = os.environ.get("KB_S_MSG")
 
+CHAT_ID = os.environ.get("CHAT_ID")
+NOTIFY_SERVER_ERR = os.environ.get("NOTIFY_SERVER_ERR")
+NOTIFY_SERVER =  os.environ.get("NOTIFY_SERVER")
+NOTIFY_F_ERR = os.environ.get("NOTIFY_F_ERR")
+NOTIFY_S_ERR = os.environ.get("NOTIFY_S_ERR")
+
+KB_CATALOG_MSG = os.environ.get("KB_CATALOG_MSG")
+CATALOG_MSG = os.environ.get("CATALOG_MSG")
 
 try:
     from telegram import __version_info__
@@ -50,39 +58,48 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if os.path.exists(f"{F_PATH}"):
         f_check = F_MSG
     else:
         f_check = F_ERR_MSG
 
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={NOTIFY_F_ERR}"
+        print(requests.get(url).json()) # this sends the message        
+
     if os.path.exists(f"{S_PATH}"):
         s_check = S_MSG
     else:
         s_check = S_ERR_MSG
 
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={NOTIFY_S_ERR}"
+        print(requests.get(url).json()) # this sends the message
+
     # hostname = "{SERVER_IP}"
     response = os.system("ping -c 1 " + hostname)
     # and then check the response...
     if response == 0:
-        pingstatus = SERVER_STATUS_MSG
+        pingstatus = text=f"{SERVER_STATUS_MSG}"
     else:
-        pingstatus = SERVER_ERR_STATUS_MSG
-
-
+        pingstatus = text=f"{SERVER_ERR_STATUS_MSG}"
+        
     """Sends a message with three inline buttons attached."""
     keyboard = [
         [
-            InlineKeyboardButton(KB_SERVER_MSG, callback_data=pingstatus),
-            InlineKeyboardButton(KB_F_MSG, callback_data=f_check),
+            InlineKeyboardButton(KB_SERVER_MSG, callback_data=f"{pingstatus}"),
+            InlineKeyboardButton(KB_F_MSG, callback_data=f"{f_check}"),
         ],
-        [InlineKeyboardButton(KB_S_MSG, callback_data=s_check)],
+        [
+            InlineKeyboardButton(KB_S_MSG, callback_data=f"{s_check}"),
+            InlineKeyboardButton(KB_CATALOG_MSG, callback_data=f"{CATALOG_MSG}"),
+
+
+        ],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("---- !!Aplicação em testes!! ----\nQual serviço você quer que eu verifique?", reply_markup=reply_markup)
+    await update.message.reply_text("------- !!Aplicação em testes!! -------\nQual serviço você quer que eu verifique?", reply_markup=reply_markup)
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -96,6 +113,10 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await query.edit_message_text(text=f"{query.data}")
 
 
+async def catalogo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("Catalogo: https://news.gcampos.site/catalogo-plex.html")
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Displays info on how to use the bot."""
     await update.message.reply_text("Use /lullybot para utilizar este bot.")
@@ -104,21 +125,23 @@ async def criador_command(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     """Displays info on how to use the bot."""
     await update.message.reply_text("Quem me criou foi o Glaucio Campos.")
 
-async def glauglau_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Displays info on how to use the bot."""
-    await update.message.reply_text("Não, aqui é a Lully.")
-
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
 #    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("lully", start))
+    application.add_handler(CommandHandler("lully", start)) 
     application.add_handler(CommandHandler("lullybot", start))
     application.add_handler(CommandHandler("ajuda", help_command))
-    application.add_handler(CommandHandler("problema", help_command))
     application.add_handler(CallbackQueryHandler(button))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("catalogo", catalogo))
     application.add_handler(CommandHandler("criador", criador_command))
-    application.add_handler(CommandHandler("glauglau", glauglau_command))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
